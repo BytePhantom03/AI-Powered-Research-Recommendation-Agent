780f1c9ae1d0683391f5debddf5a2e9a88a24322
BytePhantom03 <ravikumarzapadiya@gmail.com>
2026-06-04 21:44:11 +0530

    chore: remove test scripts

--- test_fpdf5.py
+++
@@ -1,9 +0,0 @@
-from fpdf import FPDF
-
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font("helvetica", "", 11)
-
-y1 = pdf.get_y()
-pdf.multi_cell(0, 8, txt="Hello world\nSecond line")
-print(f"Y before: {y1}, Y after: {pdf.get_y()}, X after: {pdf.get_x()}")
